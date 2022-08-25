65bbe32e2827e9d16af4b542ceee987c78947d79
Giovanne Azevedo Marrone <182200226@senacinformatica.br>
2022-08-25 16:22:33 -0300

    server usage

--- app.js
+++ app.js
@@ -11,12 +11,26 @@ app.use(express.static(`${__dirname}/public`))
 app.get('/transacoes', (req, res) => {
   const repositorio = new TransacoesRepositorio()
   const transacoes =  repositorio.listarTransacoes()
+
+  let saldo = 0
+  transacoes.transacoes.forEach((transacao) => {
+    if(transacao.categoria === "Despesa") {
+      saldo = saldo - transacao.valor
+    }
+    if(transacao.categoria === "Receita") {
+      saldo = saldo + transacao.valor
+    }
+  })
+
+  transacoes.saldo = saldo
+
   res.send(transacoes)
 })
 
 app.post('/transacoes', (req, res) => {
   const repositorio = new TransacoesRepositorio()
   const transacao = req.body
+  console.log(req.body)
   repositorio.criarTransacao(transacao)
   res.status(201).send(transacao)
 })
