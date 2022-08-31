63073fe04c0b39598f024a37675a226afc6c2dd6
Giovanne Marrone <giovannemarrone@gmail.com>
2022-08-31 20:13:32 -0300

    removed comments

--- public/main.js
+++ public/main.js
@@ -1,80 +1,6 @@
-/*
-    * * * * * * * *
-   DESAFIO PLANILHA DE GASTOS:
-
-    Nesse exercício vamos integrar com dados da API ao invés de trabalhar
-    com um objeto local.
-    
-    1. Primeiramente é necessário criar uma função que chama um GET na seguinte
-    API: https://run.mocky.io/v3/ba2007f7-04ea-465b-985e-b16c11e8061d
-    
-    O resultado será usado para preencher o saldo e transacoes na tela.
-    Use as funções criadas no exercício anterior para preencher os dados na tela,
-    a única diferença é que agora a fonte dos dados é uma API e não o objeto
-    local.
-    
-    2. Nas funções de clique da receita e despesa, não precisaremos mais atribuir
-    valores ao objeto local. Agora precisaremos integrar com uma API para enviar 
-    os dados digitados pelo usuário. Então integre com a seguinte API: 
-    https://run.mocky.io/v3/c50981e7-1c9b-471c-9128-3adf362cf36f
-    Envie nessa API um POST com os dados digitados pelo usuário em um objeto
-    no formato: { descricao: '', valor: 0 }
-    Toda a validação de número continua valendo.
-    
-    3. Depois de enviar os dados digitados pelo cliente para a API é necessário
-    buscar novamente os dados para exibir na tela. Deve-se chamar a função de
-    busca de dados ao final da função de clique dos botões de despesa e receita.
-    
-    * * * * * * * *
-*/
-/*
-const financas = {
-  saldo: 10,
-  transacoes: [
-    {
-      descricao: 'Salgado na faculdade',
-      categoria: 'Despesa',
-      valor: 5.5
-    },
-    {
-      descricao: 'Livro Clean Code',
-      categoria: 'Despesa',
-      valor: 50
-    },
-    {
-      descricao: 'Grana do estágio',
-      categoria: 'Receita',
-      valor: 80
-    },
-    {
-      descricao: 'Capinha pro celular',
-      categoria: 'Despesa',
-      valor: 15
-    },
-  ]
-};
-*/
-/*
-    !!BÔNUS!!
-    Essa função recebe como parâmetro um número e o retorna formatado
-    como moeda, assim a informação fica mais legível na tela ;)
-*/
 function formatarValor(valor) {
   return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 }
-
-/*function exibirFinancas() {
-  console.log('Saldo: ', formatarValor(financas.saldo));
-  financas.transacoes.forEach((transacao, i) => {
-    console.log('');
-    
-    console.log('Transacao ', i + 1, ':');
-    console.log('[', transacao.categoria, ']', transacao.descricao);
-    console.log(formatarValor(transacao.valor));
-  });
-}*/
-
-// exibirFinancas();
 
 function adicionarDespesa() {
   const descricaoDespesa = window.prompt("Qual a descricao de sua despesa?");
@@ -100,11 +26,6 @@ function adicionarDespesa() {
     categoria: "Despesa",
   };
 
-  // financas.transacoes.push(despesa);
-  // financas.saldo = financas.saldo - valor;
-
-  // setSaldo();
-  // adicionaTransacoes();
   enviaDados(despesa);
   getTransacoes();
 }
@@ -133,10 +54,6 @@ function adicionarReceita() {
     categoria: "Receita",
   };
 
-  // financas.transacoes.push(receita);
-  // financas.saldo = financas.saldo + valor;
-  // setSaldo();
-  // adicionaTransacoes();
   enviaDados(receita);
   getTransacoes();
 }
@@ -154,10 +71,6 @@ async function removeDados() {
   getTransacoes()
 }
 
-// adicionarDespesa();
-// adicionarReceita();
-
-// exibirFinancas();
 
 function setSaldo(saldo) {
   document.getElementById("saldo").innerHTML = `Saldo: ${Intl.NumberFormat("pt-BR", {
